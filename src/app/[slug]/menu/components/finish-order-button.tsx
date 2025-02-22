cb0cb2bcf729b6b38f553f57890600f853e8e4e9
GuilhermeEK <guilhermeelizeu23@gmail.com>
2025-02-22 19:16:52 -0300

    feat: add initial finish order component

---
+++ src/app/[slug]/menu/components/finish-order-button.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { PatternFormat } from "react-number-format";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { isValidCpf } from "../helpers/cpf";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome é obrigatório.",
+  }),
+  cpf: z
+    .string()
+    .trim()
+    .min(1, { message: "CPF é obrigatório." })
+    .refine((value) => isValidCpf(value), {
+      message: "CPF inválido.",
+    }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+const FinishOrderButton = () => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      cpf: "",
+    },
+    shouldUnregister: true,
+  });
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button className="w-full rounded-full">Finalizar Pedido</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Finalizar Pedido</DrawerTitle>
+          <DrawerDescription>
+            Insira suas informações abaixo para finalizar o seu pedido.
+          </DrawerDescription>
+        </DrawerHeader>
+        <Form {...form}>
+          <div className="p-5">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome..." {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="cpf"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu CPF</FormLabel>
+                    <FormControl>
+                      <PatternFormat
+                        placeholder="Digite seu CPF..."
+                        format="###.###.###-##"
+                        customInput={Input}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <DrawerFooter>
+                <Button
+                  type="submit"
+                  variant="destructive"
+                  className="rounded-full"
+                >
+                  Finalizar
+                </Button>
+                <DrawerClose asChild>
+                  <Button variant="outline" className="w-full rounded-full">
+                    Cancelar
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </form>
+          </div>
+        </Form>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default FinishOrderButton;
