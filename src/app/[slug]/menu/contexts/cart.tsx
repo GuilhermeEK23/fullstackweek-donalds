cceea689cd50ad7d7559719a506c18676e48faeb
GuilhermeEK <guilhermeelizeu23@gmail.com>
2025-02-22 23:33:20 -0300

    feat: ensure cart summary is show on restaurant page

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,6 +11,7 @@ export interface CartProduct
 interface ICartContext {
   isOpen: boolean;
   total: number;
+  totalQuantity: number;
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
@@ -22,6 +23,7 @@ interface ICartContext {
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   total: 0,
+  totalQuantity: 0,
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
@@ -36,6 +38,10 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const total = products.reduce((acc, product) => {
     return acc + product.price * product.quantity;
+  }, 0);
+
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
   }, 0);
 
   const toggleCart = () => {
@@ -111,6 +117,7 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       value={{
         isOpen,
         total,
+        totalQuantity,
         products,
         toggleCart,
         addProduct,
